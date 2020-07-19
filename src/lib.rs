da4aaf8d26af508da96448a41df2c877da436407
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-19 15:14:05 -0700

    attributes cant be overloaded as function-like, they have different signatures

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,24 @@
 use proc_macro::{TokenStream};
 use quote::{quote};
 
+#[proc_macro_attribute]
+pub fn dot(attr: TokenStream, item: TokenStream) -> TokenStream {
+    println!("dot attr: {}", attr);
+    println!("dot item: {}", item);
+
+    let expanded = quote! {
+        {
+            let mut stream = String::new();
+            stream
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
 #[proc_macro]
 pub fn dot_html(input: TokenStream) -> TokenStream {
-    println!("{}", input);
+    println!("dot_html: {}", input);
 
     let expanded = quote! {
         {
