4f7eb74407747cd9b1132e62fda29fd337d3506f
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-22 15:45:25 -0700

    moving onto dot_html! macro

--- src/dot_html.rs
+++ src/dot_html.rs
@@ -1,17 +1,24 @@
 use quote::{quote_spanned, ToTokens};
 //use proc_macro2::{Span};
 use syn::parse::{Parse, ParseStream, Result};
+use syn::{Path,Ident,Expr,parse_quote};
 use rdxl_internals::xhtml::Xhtml;
 
 pub struct DotHtmlInvocation {
+   template: Path,
+   kwargs: Vec<(Ident,Expr)>,
    xhtml: Xhtml
 }
 
 impl Parse for DotHtmlInvocation {
     fn parse(input: ParseStream) -> Result<Self> {
+        let template: Path = parse_quote!(crate::template::default);
+        let kwargs = Vec::new();
         let xhtml: Xhtml = input.parse()?;
 
         Ok(DotHtmlInvocation {
+            template: template,
+            kwargs: kwargs,
             xhtml: xhtml,
         })
     }
