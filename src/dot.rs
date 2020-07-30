7ac36d06588ae31d62646a5183671b69a9379df5
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-29 17:16:06 -0700

    remove circular dependency

--- src/dot.rs
+++ src/dot.rs
@@ -10,7 +10,13 @@ pub fn quote_itemfn(f: syn::ItemFn) -> TokenStream {
       #[ctor::ctor]
       fn #fctor() {
          let fname = format!("{}/{}.html", std::module_path!().replace("::","/"), #fi_s);
-         rdxl_static::dot_to_file(fname, #fi()).expect("dot_to_file failed");
+         let dat = #fi();
+         let pr = fname.rfind("/").expect("dot_to_file expected a / in directory path");
+         let pd = fname[..pr].to_string();
+         let p = std::path::Path::new(&fname);
+         std::fs::create_dir_all(pd).expect("dot_to_file could not create directory");
+         let mut f = std::fs::File::create(p).expect("dot_to_file could not create file");
+         std::io::Write::write_all(&mut f, dat.as_bytes()).expect("dot_to_file could not write bytes to file");
       }
    }
 }
